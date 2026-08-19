a4c1617c916b73a2ec4f29282d66ddf84a089922
MuneerAhmadDev <munirahmedst268@gmail.com>
2026-08-19 21:03:29 +0500

    Update API_BASE URL for production environment

--- src/api/api.js
+++ src/api/api.js
@@ -8,7 +8,7 @@ import Cookies from 'js-cookie';
 const API_BASE =
   process.env.NEXT_PUBLIC_API_URL ??
   (process.env.NODE_ENV === 'production'
-    ? 'https://surgimedsupplies.com/aero-api'
+    ? 'https://api.myonlinequran.co.uk/'
     : 'http://localhost:8000');
 
 const api = axios.create({
